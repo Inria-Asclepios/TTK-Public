326c6f5e48b55664fa24063393a3e2e7a0a5c370
filus <filus@34385dc9-8395-4963-8648-45654084226b>
2010-03-26 17:59:26 +0000

    Adding copyright info
    git-svn-id: svn://scm.gforge.inria.fr/svn/parietal-public/Utilities/ITKProgramFactory@6 34385dc9-8395-4963-8648-45654084226b

--- itkCommandObjectBase.h
+++ itkCommandObjectBase.h
@@ -1,3 +1,19 @@
+/*=========================================================================
+
+  Program:   ITK Program Factory
+  Module:    $RCSfile: $
+  Language:  C++
+  Date:      $Date: $
+  Version:   $Revision: $
+
+  Copyright (c) INRIA Saclay Île-de-France, Parietal Research Team. All rights reserved.
+  See CodeCopyright.txt for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
 #ifndef _itk_CommandObjectBase_h_
 #define _itk_CommandObjectBase_h_
 
--- itkCommandObjectFactory.cxx
+++ itkCommandObjectFactory.cxx
@@ -1,3 +1,19 @@
+/*=========================================================================
+
+  Program:   ITK Program Factory
+  Module:    $RCSfile: $
+  Language:  C++
+  Date:      $Date: $
+  Version:   $Revision: $
+
+  Copyright (c) INRIA Saclay Île-de-France, Parietal Research Team. All rights reserved.
+  See CodeCopyright.txt for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
 #include "itkCommandObjectFactory.h"
 
 #include "itkMutexLock.h"

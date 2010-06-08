47abf878c91f889b314efd3a95dd28edea796a3a
filus <filus@0a38917d-65eb-41a1-bb20-5b51c338416a>
2010-06-08 08:34:27 +0000

    Better layout of the license text
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/ttk/trunk@75 0a38917d-65eb-41a1-bb20-5b51c338416a

--- Commands/ttk-convert.cxx
+++ Commands/ttk-convert.cxx
@@ -38,17 +38,19 @@ int main (int narg, char *args[])
   itk::ConsolidateFiberBundleCommandFactory::RegisterOneFactory();
 
   if (narg<2) {
-    std::cout << "Software Tensor ToolKit ©INRIA 2010, version 1.0.0\n";
+    std::cout << "Software Tensor ToolKit (c)INRIA 2010, version 1.0.0\n";
     std::cout << "\n";
     std::cout << "Author: Pierre Fillard (Pierre.Fillard@inria.fr)\n";
     std::cout << "\n";
-    std::cout << "INRIA freely grants the non-exclusive right to use the Software for RESEARCH PURPOSES\n"
-      "ONLY. Every user of the Software will communicate to the Asclepios team (pierre.fillard@inria.fr)\n"
-      "their remarks as to the use of the Software. Please note that the Software implements the\n"
-      "following patent: \"Improved device for processing raw images or tensor images\", Arsigny\n"
-      "V., Pennec X., Fillard P., Ayache N., 2006.04.07, EP1866866, WO2006106242.\n\n"
-      "THUS, NO COMMERCIAL USE, EXPLOITATION NOR DISTRIBUTION IS AUTHORIZED BY THIS LICENSE. IF\n"
-      "SUCH USE IS DESIRED, PLEASE CONTACT THE TECH TRANSFER OFFICE (rev-ppi@sophia.inria.fr).\n\n";
+    std::cout << "INRIA freely grants the non-exclusive right to use the Software for RESEARCH\n"
+                 "PURPOSES ONLY. Every user of the Software will communicate to the Asclepios\n"
+                 "team (pierre.fillard@inria.fr) their remarks as to the use of the Software.\n"
+                 "Please note that the Software implements the following patent: \"Improved\n"
+                 "device for processing raw images or tensor images\", Arsigny V., Pennec X.,\n"
+				 "Fillard P., Ayache N., 2006.04.07, EP1866866, WO2006106242.\n\n"
+                 "THUS, NO COMMERCIAL USE, EXPLOITATION NOR DISTRIBUTION IS AUTHORIZED BY THIS\n"
+                 "LICENSE. IF SUCH USE IS DESIRED, PLEASE CONTACT THE TECH TRANSFER OFFICE\n"
+				 "(rev-ppi@sophia.inria.fr).\n\n";
     std::cout << "Available commands:\n";
     itk::CommandObjectFactory::PrintHelp( std::cout, 0 );
     return EXIT_FAILURE;

8ef23991351ca4d4240a712fb78c21db8a85f426
ntoussaint <ntoussaint@0a38917d-65eb-41a1-bb20-5b51c338416a>
2010-07-02 14:56:35 +0000

    Nicolas Toussaint: Adding dimensions 4 5 and 6 to the image conversion tool, also cleaning the macro, and reading the information can be done without actual reading, but rather generateinformation method
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/ttk/trunk@79 0a38917d-65eb-41a1-bb20-5b51c338416a

--- Commands/itkImageConverterCommand.cxx
+++ Commands/itkImageConverterCommand.cxx
@@ -1,10 +1,10 @@
 /*=========================================================================
 
   Program:   Tensor ToolKit - TTK
-  Module:    $URL:$
+  Module:    $URL$
   Language:  C++
-  Date:      $Date:$
-  Version:   $Revision:$
+  Date:      $Date$
+  Version:   $Revision$
 
   Copyright (c) INRIA 2010. All rights reserved.
   See LICENSE.txt for details.
@@ -29,26 +29,34 @@
 #include "mipsInrimageImageIOFactory.h"
 #endif
 
-// macro for templated ITK read and write
-#define ITK_READ_WRITE \
-typedef itk::ImageFileReader<RealImageType> RealReaderType; \
-RealReaderType::Pointer RealReader = RealReaderType::New(); \
-RealReader->SetFileName(filename); \
-try { RealReader->Update(); } \
-catch(itk::ExceptionObject &e)  { \
-std::cerr << e; \
-return -1; \
-} \
-typedef itk::ImageFileWriter<RealImageType> WriterType; \
-WriterType::Pointer writer = WriterType::New(); \
-writer->SetFileName(arg[2]); \
-writer->SetInput(RealReader->GetOutput()); \
-try { writer->Update(); } \
-catch(itk::ExceptionObject &e) { \
-std::cerr << e; \
-return -1; \
-}
 
+#ifdef ITK_USE_REVIEW
+#include <itkPhilipsRECImageIOFactory.h>
+#endif
+
+// macro for templated ITK read and write
+#define ReadWriteImageMacro(dimension, type)				\
+  itk::ImageFileReader< itk::Image<type, dimension> >::Pointer Reader = itk::ImageFileReader< itk::Image<type, dimension> >::New(); \
+  Reader->SetFileName ( filename1 );					\
+  try									\
+  {									\
+    Reader->Update();							\
+  }									\
+  catch (itk::ExceptionObject &e) {					\
+    std::cerr << e;							\
+    return -1;								\
+  }									\
+  itk::ImageFileWriter< itk::Image<type, dimension> >::Pointer Writer = itk::ImageFileWriter< itk::Image<type, dimension> >::New(); \
+  Writer->SetFileName ( filename2 );					\
+  Writer->SetInput(Reader->GetOutput());				\
+  try									\
+  {									\
+    Writer->Update();							\
+  }									\
+  catch (itk::ExceptionObject &e) {					\
+    std::cerr << e;							\
+    return -1;								\
+  }									\
 
 namespace itk
 {
@@ -73,134 +81,125 @@ namespace itk
       return -1;
     }
   
-    const char* filename = arg[1];
+    const char* filename1 = arg[1];
+    const char* filename2 = arg[2];
     
     itk::AnalyzeImageIOFactory::RegisterOneFactory();
 #ifdef TTK_USE_MIPS
     itk::InrimageImageIOFactory::RegisterOneFactory();
 #endif
+#ifdef ITK_USE_REVIEW
+  itk::PhilipsRECImageIOFactory::RegisterOneFactory();
+#endif
   
-    // dummy reading to get type and dimension
-    typedef itk::Image<float,3> ImageType;  
+    // dummy reading information to get type and dimension
+    typedef itk::Image<float,3> ImageType;
     typedef itk::ImageFileReader<ImageType> ReaderType;
-    ReaderType::Pointer reader = ReaderType::New();
-    reader->SetFileName(filename);
-    try { reader->Update(); }
-    catch(itk::ExceptionObject &e) {
+    ReaderType::Pointer informationreader = ReaderType::New();
+    informationreader->SetFileName(filename1);
+    try
+    {
+      informationreader->GenerateOutputInformation();
+    }
+    catch(itk::ExceptionObject &e)
+    {
       std::cerr << e;
       return -1;
     }
     
-    std::cerr << "Component Type: " << reader->GetImageIO()->GetComponentTypeAsString(reader->GetImageIO()->GetComponentType()) << std::endl;
-    std::cerr << "Pixel Type: " << reader->GetImageIO()->GetPixelTypeAsString(reader->GetImageIO()->GetPixelType()) << std::endl;
-    if(reader->GetImageIO()->GetPixelType() != itk::ImageIOBase::SCALAR) {
+    std::cerr << "Dimension: " << informationreader->GetImageIO()->GetNumberOfDimensions() << std::endl;
+    std::cerr << "Component Type: " << informationreader->GetImageIO()->GetComponentTypeAsString(informationreader->GetImageIO()->GetComponentType()) << std::endl;
+    std::cerr << "Pixel Type: " << informationreader->GetImageIO()->GetPixelTypeAsString(informationreader->GetImageIO()->GetPixelType()) << std::endl;
+
+    if(informationreader->GetImageIO()->GetPixelType() != itk::ImageIOBase::SCALAR)
+    {
       std::cerr << "Only scalar images can be converted" << std::endl;
       return -1;
     }
     
     // real reading with dimension and type
-    unsigned int dim = reader->GetImageIO()->GetNumberOfDimensions();
-    
-    if (dim==3) {
-      if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::UNKNOWNCOMPONENTTYPE) {
-	std::cerr << "Unknown type, exiting" << std::endl;
-	return -1;
-      }
-      else if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::UCHAR) {
-	typedef itk::Image<unsigned char,3> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else if (reader->GetImageIO()->GetComponentType() ==  itk::ImageIOBase::CHAR) {
-	typedef itk::Image<char,3> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::USHORT) {
-	typedef itk::Image<unsigned short,3> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::SHORT) {
-	typedef itk::Image<short,3> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::UINT) {
-	typedef itk::Image<unsigned int,3> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::INT){
-	typedef itk::Image<int,3> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::ULONG) {
-	typedef itk::Image<unsigned long,3> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::LONG) {
-	typedef itk::Image<long,3> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::FLOAT) {
-	typedef itk::Image<float,3> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::DOUBLE) {
-	typedef itk::Image<double,3> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else {
-	typedef itk::Image<float,3> RealImageType;
-	ITK_READ_WRITE
-	  }
-    }
-    
-    else if (dim==2) {
-      if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::UNKNOWNCOMPONENTTYPE) {
-	std::cerr << "Unknown type, exiting" << std::endl;
-	return -1;
-      }
-      else if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::UCHAR) {
-	typedef itk::Image<unsigned char,2> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::CHAR) {
-	typedef itk::Image<char,2> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::USHORT) {
-	typedef itk::Image<unsigned short,2> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::SHORT) {
-	typedef itk::Image<short,2> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::UINT) {
-	typedef itk::Image<unsigned int,2> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::INT) {
-	typedef itk::Image<int,2> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::ULONG) {
-	typedef itk::Image<unsigned long,2> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::LONG) {
-	typedef itk::Image<long,2> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::FLOAT) {
-	typedef itk::Image<float,2> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else if (reader->GetImageIO()->GetComponentType() == itk::ImageIOBase::DOUBLE) {
-	typedef itk::Image<double,2> RealImageType;
-	ITK_READ_WRITE
-	  }
-      else {
-	typedef itk::Image<float,2> RealImageType;
-	ITK_READ_WRITE
-	  }
+    unsigned int dim = informationreader->GetImageIO()->GetNumberOfDimensions();
+    itk::ImageIOBase::IOComponentType componenttype = informationreader->GetImageIO()->GetComponentType();
+
+    if (dim > 6)
+    { std::cerr << "Dimension Too High." << dim<< std::endl; return -1; }
+
+    switch (componenttype)
+    {
+	case itk::ImageIOBase::UCHAR:
+	  if      (dim == 2) { ReadWriteImageMacro (2, unsigned char); }
+	  else if (dim == 3) { ReadWriteImageMacro (3, unsigned char); }
+	  else if (dim == 4) { ReadWriteImageMacro (4, unsigned char); }
+	  else if (dim == 5) { ReadWriteImageMacro (5, unsigned char); }
+	  else if (dim == 6) { ReadWriteImageMacro (6, unsigned char); }
+	  break;
+	case itk::ImageIOBase::CHAR:
+	  if      (dim == 2) { ReadWriteImageMacro (2, char); }
+	  else if (dim == 3) { ReadWriteImageMacro (3, char); }
+	  else if (dim == 4) { ReadWriteImageMacro (4, char); }
+	  else if (dim == 5) { ReadWriteImageMacro (5, char); }
+	  else if (dim == 6) { ReadWriteImageMacro (6, char); }
+	  break;
+    	case itk::ImageIOBase::USHORT:
+	  if      (dim == 2) { ReadWriteImageMacro (2, unsigned short); }
+	  else if (dim == 3) { ReadWriteImageMacro (3, unsigned short); }
+	  else if (dim == 4) { ReadWriteImageMacro (4, unsigned short); }
+	  else if (dim == 5) { ReadWriteImageMacro (5, unsigned short); }
+	  else if (dim == 6) { ReadWriteImageMacro (6, unsigned short); }
+	  break;
+    	case itk::ImageIOBase::SHORT:
+	  if      (dim == 2) { ReadWriteImageMacro (2, short); }
+	  else if (dim == 3) { ReadWriteImageMacro (3, short); }
+	  else if (dim == 4) { ReadWriteImageMacro (4, short); }
+	  else if (dim == 5) { ReadWriteImageMacro (5, short); }
+	  else if (dim == 6) { ReadWriteImageMacro (6, short); }
+	  break;
+    	case itk::ImageIOBase::UINT:
+	  if      (dim == 2) { ReadWriteImageMacro (2, unsigned int); }
+	  else if (dim == 3) { ReadWriteImageMacro (3, unsigned int); }
+	  else if (dim == 4) { ReadWriteImageMacro (4, unsigned int); }
+	  else if (dim == 5) { ReadWriteImageMacro (5, unsigned int); }
+	  else if (dim == 6) { ReadWriteImageMacro (6, unsigned int); }
+	  break;
+    	case itk::ImageIOBase::INT:
+	  if      (dim == 2) { ReadWriteImageMacro (2, int); }
+	  else if (dim == 3) { ReadWriteImageMacro (3, int); }
+	  else if (dim == 4) { ReadWriteImageMacro (4, int); }
+	  else if (dim == 5) { ReadWriteImageMacro (5, int); }
+	  else if (dim == 6) { ReadWriteImageMacro (6, int); }
+	  break;
+    	case itk::ImageIOBase::ULONG:
+	  if      (dim == 2) { ReadWriteImageMacro (2, unsigned long); }
+	  else if (dim == 3) { ReadWriteImageMacro (3, unsigned long); }
+	  else if (dim == 4) { ReadWriteImageMacro (4, unsigned long); }
+	  else if (dim == 5) { ReadWriteImageMacro (5, unsigned long); }
+	  else if (dim == 6) { ReadWriteImageMacro (6, unsigned long); }
+	  break;
+	case itk::ImageIOBase::LONG:
+	  if      (dim == 2) { ReadWriteImageMacro (2, long); }
+	  else if (dim == 3) { ReadWriteImageMacro (3, long); }
+	  else if (dim == 4) { ReadWriteImageMacro (4, long); }
+	  else if (dim == 5) { ReadWriteImageMacro (5, long); }
+	  else if (dim == 6) { ReadWriteImageMacro (6, long); }
+	  break;
+	case itk::ImageIOBase::FLOAT:
+	  if      (dim == 2) { ReadWriteImageMacro (2, float); }
+	  else if (dim == 3) { ReadWriteImageMacro (3, float); }
+	  else if (dim == 4) { ReadWriteImageMacro (4, float); }
+	  else if (dim == 5) { ReadWriteImageMacro (5, float); }
+	  else if (dim == 6) { ReadWriteImageMacro (6, float); }
+	  break;
+    	case itk::ImageIOBase::DOUBLE:
+	  if      (dim == 2) { ReadWriteImageMacro (2, double); }
+	  else if (dim == 3) { ReadWriteImageMacro (3, double); }
+	  else if (dim == 4) { ReadWriteImageMacro (4, double); }
+	  else if (dim == 5) { ReadWriteImageMacro (5, double); }
+	  else if (dim == 6) { ReadWriteImageMacro (6, double); }
+	  break;
+    	default:
+    	  std::cerr << "Unrecognized Component Type : " << informationreader->GetImageIO()->GetComponentTypeAsString(componenttype) << std::endl;
+    	  return -1;
+    	  break;  
     }
     
     return 0;
